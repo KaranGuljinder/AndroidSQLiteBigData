8737d6a9655da21c2581b011bec3b062e892cf9c
GULJINDER Karan <KGULJINDER@fr.scc.com>
2019-04-18 10:38:14 +0200

    Add Requete SQL FindWord

--- app/src/main/java/com/developpement/guide/myapplication/MainActivity.kt
+++ app/src/main/java/com/developpement/guide/myapplication/MainActivity.kt
@@ -1,12 +1,41 @@
 package com.developpement.guide.myapplication
 
+
+import android.database.sqlite.SQLiteDatabase
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-}
+
+        val buttonOneGram = findViewById<Button>(R.id.onegram)
+        val buttonTwoGram = findViewById<Button>(R.id.twogram)
+        val buttonThreeGram = findViewById<Button>(R.id.threegram)
+        val messageText = findViewById<EditText>(R.id.edittext)
+        val dbHandler = MyDBHandler(this, null, null, 1)
+
+        buttonOneGram.setOnClickListener {
+            var word : String
+            word = messageText.text.toString()
+            var word1 : String
+            word1 = dbHandler.findOneGram(word).toString()
+            messageText.setText(word + word1)
+            
+    }
+
+        buttonTwoGram.setOnClickListener {
+
+        }
+
+        buttonThreeGram.setOnClickListener{
+
+        }
+
+    }
 }
--- app/src/main/java/com/developpement/guide/myapplication/MyDBHandler.kt
+++ app/src/main/java/com/developpement/guide/myapplication/MyDBHandler.kt
@@ -6,28 +6,25 @@ import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
     private val DATABASE_VERSION = 1
-    private val DATABASE_NAME = "dataDB.db"
-    val TABLE_MESSENGER = "messsenger"
-    const val COLUMN_ID = "_id"
+    private val DATABASE_NAME = "db.sqlite"
+    val TABLE_MESSENGER = "a"
     const val COLUMN_COL1 = "col1"
     const val COLUMN_COL2 = "col2"
     const val COLUMN_COL3 = "col3"
-    const val COLUMN_COUNTER = "counter"
+    const val COLUMN_COUNTER = "COUNTER"
 
 class MyDBHandler(context: Context, name: String?, factory: SQLiteDatabase.CursorFactory?, version: Int) :
     SQLiteOpenHelper(context, DATABASE_NAME, factory, DATABASE_VERSION) {
-
     override fun onCreate(db: SQLiteDatabase) {
-        val CREATE_PRODUCTS_TABLE = ("CREATE TABLE " +
+        val CREATE_MESSENGER_TABLE = ("CREATE TABLE " +
                 TABLE_MESSENGER + "("
-                + COLUMN_ID + " INTEGER PRIMARY KEY," +
-                COLUMN_COL1
+                + COLUMN_COL1
                 + " TEXT," +
                 COLUMN_COL2
                 + " TEXT," +
                 COLUMN_COL3
                 + " TEXT," + COLUMN_COUNTER + " INTEGER" + ")")
-        db.execSQL(CREATE_PRODUCTS_TABLE)
+        db.execSQL(CREATE_MESSENGER_TABLE)
     }
 
     override fun onUpgrade(
@@ -38,16 +35,21 @@ class MyDBHandler(context: Context, name: String?, factory: SQLiteDatabase.Curso
         onCreate(db)
     }
 
-    fun addProduct(product: Test) {
-        val values = ContentValues()
-        values.put(COLUMN_COL1, product.col1)
-        values.put(COLUMN_COL2, product.col2)
-        values.put(COLUMN_COL3, product.col3)
-        values.put(COLUMN_COUNTER, product.counter)
+    fun findOneGram(word: String) : String ?  {
 
+        val query =
+            "SELECT $COLUMN_COL2 FROM  $TABLE_MESSENGER  WHERE $COLUMN_COL1  = \"$word\" ORDER BY $COLUMN_COUNTER desc LIMIT 3"
         val db = this.writableDatabase
+        val cursor = db.rawQuery(query, null)
+        var data: Test? = null
 
-        db.insert(TABLE_MESSENGER, null, values)
+        if (cursor.moveToFirst()) {
+            cursor.moveToFirst()
+
+            val string = cursor.getString(0)
+            cursor.close()
+        }
         db.close()
+        return data?.col2
     }
 }
